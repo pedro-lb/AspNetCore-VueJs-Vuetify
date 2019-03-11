3125b36b373ae8a20a28664aa25ea0601efbd8bd
pedrobini <pedro1451@gmail.com>
2019-03-11 16:04:59 -0300

    Controller cleanup

--- AspNetCore-VueJs-Vuetify/Controllers/HomeController.cs
+++ AspNetCore-VueJs-Vuetify/Controllers/HomeController.cs
@@ -11,11 +11,6 @@ namespace AspNetCore_VueJs_Vuetify.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
